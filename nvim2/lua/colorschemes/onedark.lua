05f2a49c26b4a766614040c3454a4e9f3f78df02
Giannis Dimoulios <giannis.dimoulios@gmail.com>
2022-04-23 15:55:34 +0300

    lol

--- nvim2/lua/colorschemes/onedark.lua
+++ nvim2/lua/colorschemes/onedark.lua
@@ -3,6 +3,9 @@ require("onedarkpro").setup({
     style = 'deep', 
     colors = {
         bg = "#000001"
+    }, 
+    options = {
+        cursorline = true
     }
 })
 require("onedarkpro").load() 
