56714ba8f5218ea6ba8a4792309e8994899c908d
Giannis Dimoulios <giannis.dimoulios@gmail.com>
2022-05-08 17:54:21 +0300

    clean up

--- Makefile
+++
@@ -1,7 +0,0 @@
-CC = gcc
-CFLAGS = -O2 -Wall -Wextra -pedantic -std=c17
-
-all: 
-	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).c
-
-
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-
-cd 
-
-mkdir -p .config/nvim/lua/packerconfig
-
-ln -s ~/dotfiles/nvim2/init.lua ~/.config/nvim/init.lua
-ln -s ~/dotfiles/nvim2/packerconfig/init.lua ~/.config/nvim/lua/packerconfig/init.lua
-
