2ebdfc24deefd89121d0605cf1fe2986f6cc8b94
Giannis <giannis.dimoulios@gmail.com>
2024-04-14 23:58:08 +0300

    remove old nvim config

--- oldnvim/lua/core/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-vim.g.moonflyTransparent = true
-
--- local status, _ = pcall(vim.cmd, "colorscheme nightfly")
-local status, _ = pcall(vim.cmd, "colorscheme moonfly")
-if not status then
-    print("Colorscheme not found.")
-    return
-end
-
