a311cf3c342ca6a6171fb92f18edd7ae86f42693
Giannis Dimoulios <giannis.dimoulios@gmail.com>
2024-05-06 11:29:29 +0300

    change git mergetool

--- nvim/lua/giannis/packer.lua
+++ nvim/lua/giannis/packer.lua
@@ -30,6 +30,7 @@ return require('packer').startup(function(use)
   use 'mbbill/undotree'
   use 'tpope/vim-fugitive'
   use 'lewis6991/gitsigns.nvim'
+  use "sindrets/diffview.nvim"
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
